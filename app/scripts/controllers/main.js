f191ae87808986c33069af5be28e9966aab5de65
Braden <braden.douglass@gmail.com>
2014-02-01 17:39:22 -0500

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('angularNodeYeomanApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
